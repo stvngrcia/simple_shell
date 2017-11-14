57c7a5a055f6f9081974bc1a90905a40bb3a889d
Steven Garcia <27@p3-4.hbtn.io>
2017-11-14 09:49:30 -0800

    adding functions that help find and execute buildin commands, exit and env are the only ones avavailable at this time

---
+++ builtins.c
@@ -0,0 +1,76 @@
+#include "olaf.h"
+
+/**
+ * env_b - Prints all the environmental variables in the current shell.
+ * @line: A string representing the input from the user.
+ */
+void env_b(__attribute__((unused))char *line)
+{
+	int i;
+	int j;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+			write(1, &environ[i][j], 1);
+		write(1, "\n", 1);
+	}
+}
+
+/**
+ * exit_b - Exits the shell. After freeing allocated resources.
+ * @line: A string representing the input from the user.
+ */
+void exit_b(char *line)
+{
+	free(line);
+	write(1, "logout\n", 8);
+	exit(1);
+}
+
+/**
+ * check_built_ins - Finds the right function needed for execution.
+ * @str: The name of the function that is needed.
+ * Return: Upon sucess a pointer to a void function. Otherwise NULL.
+ */
+void (*check_built_ins(char *str))(char *str)
+{
+	int i;
+
+	builtin_t buildin[] = {
+		{"exit", exit_b},
+		{"env", env_b},
+		{NULL, NULL}
+	};
+
+	for (i = 0; buildin[i].built != NULL; i++)
+	{
+		if (strcmp(str, buildin[i].built) == 0)
+		{
+			return (buildin[i].f);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * built_in - Checks for builtin functions.
+ * @command: An array of all the arguments passed to the shell.
+ * @line: A string representing the input from the user.
+ * Return: If function is found 0. Otherwise -1.
+ */
+int built_in(char **command, char *line)
+{
+	void (*build)(char *);
+
+	build = check_built_ins(command[0]);
+	if (build == NULL)
+		return (-1);
+	if (strcmp("exit", command[0]) == 0)
+		double_free(command);
+	build(line);
+	return (0);
+}
+
+
+

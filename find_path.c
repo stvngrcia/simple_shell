4218bea06ff15f1860bb5dbeaac95dc704a8953e
Steven Garcia <27@p3-4.hbtn.io>
2017-11-14 09:45:49 -0800

    adding functions used to find the path for commands defined on the environmental variable PATH

---
+++ find_path.c
@@ -0,0 +1,176 @@
+#include "olaf.h"
+
+/**
+ * path_finder - Acts as an interface for functions that will be able to
+ * find the full path of a program.
+ * @command: Represents a command. For example ls, echo, pwd, etc.
+ * Return: Upon sucess a string with the full path of the program.
+ * for example /bin/ls, /bin/echo, etc. Otherwise returns NULL.
+ */
+char *path_finder(char *command)
+{
+	char *str = "PATH";
+	char *constructed;
+	char **path_tokens;
+	int index;
+	char *directory;
+
+	index = find_path(str);
+	path_tokens = tokenize_path(index, str);
+	if (path_tokens == NULL)
+		return (NULL);
+
+	directory = search_directories(path_tokens, command);
+	if (directory == NULL)
+	{
+		double_free(path_tokens);
+		return (NULL);
+	}
+
+	constructed = build_path(directory, command);
+	if (constructed == NULL)
+	{
+		double_free(path_tokens);
+		return (NULL);
+	}
+
+	double_free(path_tokens);
+
+	return (constructed);
+}
+/**
+ * find_path - Finds the index of an environmental variable.
+ * @str: Environmental variable that needs to be found.
+ * Return: Upon success returns the index of the environmental variable.
+ * otherwise returns -1.
+ */
+int find_path(char *str)
+{
+	int i;
+	int len;
+	int j;
+
+	len = str_len(str);
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			if (environ[i][j] != str[j])
+				break;
+		}
+		if (j == len && environ[i][j] == '=')
+			return (i);
+	}
+	return (-1);
+}
+
+/**
+ * tokenize_path - Separates a string representing paths as an array
+ * of strings contining the path directories.
+ * @index: Index of the path in the environment variables.
+ * @str: string to separate and tokenize.
+ * Return: Upon success a NULL terminated array of pointer to strings.
+ * Otherwise returns NULL. Note!: Do not forget to free alocated
+ * memory on receiving function or when possible.
+ */
+char **tokenize_path(int index, char *str)
+{
+	char *env_var;
+	int token_count;
+	const char *delim = ":\n";
+	char **path_tokens;
+	int len;
+
+	len = str_len(str);
+	/*Moving the pointer len of str plus = sign*/
+	env_var = environ[index] + (len + 1);
+	token_count = count_token(env_var, delim);
+	if (token_count == -1)
+		return (NULL);
+	path_tokens = tokenize(token_count, env_var, delim);
+	if (path_tokens == NULL)
+		return (NULL);
+	return (path_tokens);
+}
+
+/**
+ * search_directories - Looks through directories stored in path_tokens for a
+ * specific file. aka commmand.
+ * @path_tokens: A pointer to an array of strings representing the different
+ * paths contained in the PATH environmental varible.
+ * @command: Represents a command. For example ls, echo, pwd, etc.
+ * Return: Upon success a string with the upper most directory containing
+ * the command file. Otherwise returns NULL.
+ */
+char *search_directories(char **path_tokens, char *command)
+{
+	int i, s;
+	char *cwd;
+	char *buf;
+	size_t size;
+	struct stat stat_buf;
+
+	buf = NULL;
+	size = 0;
+	cwd = getcwd(buf, size);
+	if (cwd == NULL)
+		return (NULL);
+	for (i = 0; path_tokens[i] != NULL; i++)
+	{
+		s = chdir(path_tokens[i]);
+		if (s == -1)
+		{
+			perror("ERROR!");
+			return (NULL);
+		}
+		s = stat(command, &stat_buf);
+		if (s == 0)
+		{
+			chdir(cwd);
+			free(cwd);
+			return (path_tokens[i]);
+		}
+	}
+	chdir(cwd);
+	free(cwd);
+	return (NULL);
+}
+
+/**
+ * build_path - Combines two strings one representing the path directory and
+ * one representing the command file.
+ * @directory: Represents a directory in the path.
+ * @command: Represents a file in a directory of the path.
+ * Return: Upon success a string representing the full path of a command.
+ * Otherwise NULL.
+ */
+char *build_path(char *directory, char *command)
+{
+	int i, j;
+	int dir_len;
+	int command_len;
+	int len;
+	char *built;
+
+	if (directory == NULL || command == NULL)
+		return (NULL);
+	dir_len = str_len(directory) + 1;
+	command_len = str_len(command) + 1;
+	len = dir_len + command_len;
+
+	built = malloc(sizeof(char) * len);
+	if (built == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; directory[j] != '\0'; j++, i++)
+			built[i] = directory[j];
+		built[i] = '/';
+		i++;
+		for (j = 0; command[j] != '\0'; j++, i++)
+			built[i] = command[j];
+	}
+	built[--i] = '\0';
+	return (built);
+}

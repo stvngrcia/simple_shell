9ea7064d156aca1f2b1be5ab4a7a6cbbe148a50d
Yunju Chen <info@yunjuchen.net>
2017-11-17 01:26:25 +0000

    Add _realloc

--- hbtlib.c
+++ hbtlib.c
@@ -39,3 +39,40 @@ char *_strdup(char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * _realloc - reallocate a memory block
+ * @ptr: pointer to old block
+ * @old_size: old memory size
+ * @new_size: new memory size
+ * Return: pointer to new block, NULL when failed
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *orig = (char *)ptr;
+	unsigned int i;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+	}
+	else
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; orig[i] != '\0' && i < new_size ; i++)
+			p[i] = orig[i];
+		free(ptr);
+	}
+	return (p);
+}

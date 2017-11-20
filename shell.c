4777c20a47cfa44e3035b8488adae10aee35d396
stvngrcia <stevngrcia@yahoo.com>
2017-11-20 17:22:44 +0000

    adding description for function variables

--- shell.c
+++ shell.c
@@ -2,6 +2,9 @@
 
 /**
  * main - Entry point of the program.
+ * @ac: The number of parameters passed to the executable file. In the case
+ * this variable will not be used.
+ * @av: The name of the program.
  * Return: Always 0.
  */
 int main(__attribute__((unused)) int ac, char **av)
